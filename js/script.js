2f405037c9352c100988cf46d791d5e7c6d4ac74
AnthonyBertottiFavero <bertottianthony1@gmail.com>
2023-05-03 11:50:49 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const properName = prompt (`Qual'è il tuo nome?`);
+const properSurname = prompt(`Qual'è il tuo cognome?`);
+const favColor = prompt(`Qual'è il tuo colore preferito?`);
+const favNumber = prompt (`Qual'è il tuo numero preferito?`);
+
+// const newPassword = properName + properSurname + favColor + favNumber;
+
+const newPassword = `${properName}${properSurname}${favColor}${favNumber}`;
+
+document.getElementById (`password`).innerHTML = `La tua password è: ${newPassword}`;
+
+
+const numero1 = parseInt(prompt(`Dammi un numero`));
+const numero2 = parseInt(prompt(`Dammi un altro numero`));
+
+const favNumbers = parseInt(numero1) + parseInt(numero2);
+
+document.getElementById (`fav-number`).innerHTML = `Il mio numero preferito è: ${favNumbers}`;
+
